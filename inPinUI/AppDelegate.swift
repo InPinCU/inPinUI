001ac7773748277ab038bfcda9158a82253f924e
Paras Chaudhary <paras.vc18@gmail.com>
2022-02-02 00:29:50 -0500

    second commit

--- inPinUI/AppDelegate.swift
+++ inPinUI/AppDelegate.swift
@@ -4,7 +4,7 @@
 //
 //  Created by Paras Chaudhary on 1/15/22.
 //
-
+import GoogleMaps
 import UIKit
 
 @main
@@ -13,7 +13,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        GMSServices.provideAPIKey("AIzaSyBpRalwKKTJE-5Gl0WiRdar5Zyvvt4CDkE")
         return true
     }
 
--- inPinUI/ViewController.swift
+++ inPinUI/ViewController.swift
@@ -4,16 +4,67 @@
 //
 //  Created by Paras Chaudhary on 1/15/22.
 //
-
 import UIKit
+import GoogleMaps
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,GMSMapViewDelegate,CLLocationManagerDelegate {
+    @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var addressLabel: UITextField!
+    
+    var locationManager = CLLocationManager()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        mapView.isMyLocationEnabled = true
+        mapView.delegate = self
+
+        //Location Manager code to fetch current location
+        self.locationManager.delegate = self
+        self.locationManager.startUpdatingLocation()
     }
 
+    //Location Manager delegates
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
 
+        let location = locations.last
+
+        let camera = GMSCameraPosition.camera(withLatitude: (location?.coordinate.latitude)!, longitude:(location?.coordinate.longitude)!, zoom:14)
+        mapView.animate(to: camera)
+
+        //Finally stop updating location otherwise it will come again and again in this delegate
+        self.locationManager.stopUpdatingLocation()
+
+    }
+    
+    func reverseGeocode(coordinate: CLLocationCoordinate2D) {
+      // 1
+      let geocoder = GMSGeocoder()
+
+      // 2
+      geocoder.reverseGeocodeCoordinate(coordinate) { response, error in
+          self.addressLabel.unlock()
+        guard
+          let address = response?.firstResult(),
+          let lines = address.lines
+          else {
+            return
+        }
+
+        // 3
+        self.addressLabel.text = lines.joined(separator: "\n")
+
+        // 4
+        UIView.animate(withDuration: 0.25) {
+          self.view.layoutIfNeeded()
+        }
+      }
+    }
+    func mapView(_ mapView: GMSMapView, idleAt position: GMSCameraPosition) {
+      reverseGeocode(coordinate: position.target)
+    }
+    func mapView(_ mapView: GMSMapView, willMove gesture: Bool) {
+      addressLabel.lock()
+    }
 }
+
 
